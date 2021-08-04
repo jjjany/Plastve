aad31d2901d48575954547b800f0a384f70ded44
Jana Jany Nepimachová <jjjany111@gmail.com>
2021-08-04 11:17:22 +0200

    Kreslení n vrstev pláství od svrchní vrstvy (bez ošetření ValueError)
    Pomocí modulu thurtle nakreslí n vrstev pláství (n zadává uživatel
    programu) od vrchní vrstvy ke středové.
    Na vstupu není ošetřena ValueError, kdy uživatel zadá jako vstup
    řetězec na místo čísla.

--- plastve.py
+++ plastve.py
@@ -4,50 +4,38 @@ from turtle import distance, forward, penup, pendown, exitonclick, left, right
 
 while True:
     lr = int(input('Zadej počet vrstev pláství. (Celé nezáporné číslo.)'))
-    ds = 40  # ds je číslo udávající délku strany v jednotkách želvy (pixelech?)
+    ds = 15  # ds je číslo udávající délku strany v jednotkách želvy (pixelech?)
 
     if lr < 0:
         print('Počet vrstev musí být celé nezáporné číslo.')
+    elif lr == 0:
+        print('Není, co kreslit.')
     else:
         break
 
-def pla(ds):  # tato funkce nakreslí právě 1 plástev (šestiúhelník) o délce strany ds
+def pla(ds):  # tato funkce nakreslí právě 1 vrstvu pláství, tj. 1 plástev (šestiúhelník) o délce strany ds
     for i in range(6):
         forward(ds)
         left(60)
 
+def pla_lrn(lr):  # tato funkce nakreslí n-tou vrstvu pláství pro n>=1
+    for i in range(6):
+        for i in range(lr-1):
+            pla(ds)
+            forward(ds)
+            left(60)
+            forward(ds)
+            right(60)
+        forward(ds)
+        left(60)
 
-#for i in range(lr):
 
-pla(ds)
-
-forward(ds)
-left(60)
-forward(ds)
-right(60)
-
-for i in range(5):
-    pla(ds)
-
+for i in range(lr):
+    pla_lrn(lr-i)
+    left(120)
     forward(ds)
     right(60)
-
-left(120)
-forward(ds)
-right(60)
-
-for i in range(6):
-    for i in range(2):
-        pla(ds)
-        forward(ds)
-        right(60)
-
     forward(ds)
-    left(60)
-
-forward(ds)
-left(60)
-forward(ds)
-right(60)
+    right(60)
 
 exitonclick()
